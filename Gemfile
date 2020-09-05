28a8f513eb19b1936f5bfa11866e04be456cc924
kimjh96 <kimjhs@kakao.com>
2020-09-05 14:47:11 +0900

    feat: Devise-JWT 및 라우팅 설정

--- Gemfile
+++ Gemfile
@@ -41,3 +41,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
+gem 'devise-jwt', '~> 0.7.0'
+gem 'rack-cors'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :v1 do
+    devise_for :users,
+               path: 'users',
+               path_names: {
+                 sign_in: 'sign-in',
+                 sign_out: 'sign-out',
+                 registration: 'sign-up'
+               },
+               controllers: {
+                 sessions: 'v1/users/sessions',
+                 registrations: 'v1/users/registrations'
+               }
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
