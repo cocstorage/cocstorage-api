7d08ffce0f09d551591a64b18514d0ca1ff1820a
kimjh96 <kimjhs@kakao.com>
2020-09-27 03:39:05 +0900

    fix: 저장소 개념글 댓글 목록 API 스펙 수정
    해당 댓글의 답글 목록을 포함하도록 API 스펙을 수정하였습니다.

--- app/serializers/storage_board_comment_serializer.rb
+++ app/serializers/storage_board_comment_serializer.rb
@@ -3,6 +3,7 @@ class StorageBoardCommentSerializer < ActiveModel::Serializer
   attribute :storage_board_id
   attribute :user
   attributes StorageBoardComment.column_names.reject { |name| %w[id user_id storage_board_id password created_user_agent].include? name }
+  attribute :replys
 
   def user
     user = object.user
@@ -19,5 +20,9 @@ class StorageBoardCommentSerializer < ActiveModel::Serializer
 
   def created_ip
     object.created_ip.gsub(/\.[0-9]{1,3}\.[0-9]{1,3}/, '')
+  end
+
+  def replys
+    ActiveModelSerializers::SerializableResource.new(object.storage_board_comment_replies, each_serializer: StorageBoardCommentReplySerializer)
   end
 end
