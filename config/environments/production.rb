13a4f1d4de41a76a0d33ca7e3d349487b1c51feb
kimjh96 <kimjhs@kakao.com>
2020-11-08 00:56:55 +0900

    feat: Setup Production SecretKeyBase

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.secret_key_base = Rails.application.credentials.secret_key_base
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
