00c7fb8634f4b5964c83bf46d0d1f91b5a027f61
kimjh96 <kimjhs@kakao.com>
2021-05-12 03:47:54 +0900

    fix: undefined method 에러 재수정

--- app/jobs/storage_board_scrap_job.rb
+++ app/jobs/storage_board_scrap_job.rb
@@ -57,7 +57,7 @@ class StorageBoardScrapJob < ApplicationJob
         end
 
         unless StorageBoard.where(scrap_code: scrap_code).exists?
-          storage_board = StorageBoard.create(
+          storage_board = StorageBoard.create!(
             storage_id: storage.id,
             scrap_code: scrap_code,
             source_code: storage.code,
@@ -70,114 +70,114 @@ class StorageBoardScrapJob < ApplicationJob
             is_draft: false
           )
           create_new_storage_board = true
-        end
 
-        next if storage_board.blank?
+          next if storage_board.blank?
 
-        parse_storage_board_content = Nokogiri::HTML::DocumentFragment.parse(storage_board.content)
+          parse_storage_board_content = Nokogiri::HTML::DocumentFragment.parse(storage_board.content)
 
-        images = parse_storage_board_content.css('img')
-        images.remove_attr('style')
-        images.remove_attr('onclick')
+          images = parse_storage_board_content.css('img')
+          images.remove_attr('style')
+          images.remove_attr('onclick')
 
-        images.each do |image|
-          begin
-            download_image = URI.open(image['src'], 'User-Agent' => image_user_agent, 'Referrer' => post_url)
-            storage_board.images.attach(io: download_image, filename: SecureRandom.urlsafe_base64(20))
+          images.each do |image|
+            begin
+              download_image = URI.open(image['src'], 'User-Agent' => image_user_agent, 'Referrer' => post_url)
+              storage_board.images.attach(io: download_image, filename: SecureRandom.urlsafe_base64(20))
 
-            image['src'] = storage_board.last_image_url
-            image['alt'] = 'Board Img'
-          rescue
-            next
-          end
-        end if images.present?
-
-        has_image = true if images.present?
-
-        gif_images = parse_storage_board_content.css('video')
-        gif_images.remove_attr('onmousedown')
-        gif_images.remove_attr('data-src')
-
-        gif_images.each do |gif_image|
-          begin
-            download_image = URI.open(gif_image.css('source').attr('src'), 'User-Agent' => image_user_agent, 'Referrer' => post_url)
-            storage_board.images.attach(io: download_image, filename: SecureRandom.urlsafe_base64(20))
-
-            gif_image.at('source')['src'] = storage_board.last_image_url
-            gif_image.at('source')['alt'] = 'Board Img'
-          rescue
-            next
-          end
-        end if gif_images.present?
-
-        storage_board.update(content: parse_storage_board_content, has_image: has_image)
-
-        storage_board_comment_url = "https://gall.dcinside.com/board/view/?id=#{storage.code}&no=#{scrap_code}&t=cv&exception_mode=recommend&page=1"
-
-        Selenium::WebDriver::Chrome::Service.driver_path = ENV['CHROME_DRIVER_PATH']
-
-        options = Selenium::WebDriver::Chrome::Options.new
-        options.add_argument("--user-agent=#{user_agent}")
-        options.headless!
-
-        browser = Selenium::WebDriver.for :chrome, options: options
-
-        begin
-          browser.get(storage_board_comment_url)
-
-          pages = browser.find_element(css: '.cmt_paging').find_elements(:xpath => "*")
-          page_nums = pages.map { |page| page.text.to_i }
-
-          page_nums.each do |page_num|
-            if page_num > 1
-              browser.execute_script("viewComments(#{page_num}, 'D')")
-              sleep 2
+              image['src'] = storage_board.last_image_url
+              image['alt'] = 'Board Img'
+            rescue
+              next
             end
+          end if images.present?
 
-            # Comments & Replies
-            browser.find_element(css: '.cmt_list').find_elements(:xpath => "*").each do |comment|
-              begin
-                comment_data_no = comment.attribute('id').split('_').last
-                comment_writer = comment.find_element(css: '.gall_writer.ub-writer')
-                comment_content = comment.find_element(css: '.usertxt.ub-word')
+          has_image = true if images.present?
 
-                storage_board_comment = StorageBoardComment.create(
-                  storage_board_id: storage_board.id,
-                  nickname: comment_writer.attribute('data-nick'),
-                  content: comment_content.text,
-                  created_ip: comment_writer.attribute('data-ip'),
-                  is_member: 1
-                )
+          gif_images = parse_storage_board_content.css('video')
+          gif_images.remove_attr('onmousedown')
+          gif_images.remove_attr('data-src')
 
-                # Replies
-                browser.find_elements(id: "reply_list_#{comment_data_no}").each do |reply_list|
-                  reply_list.find_elements(:xpath => "*").each do |reply|
-                    reply_writer = reply.find_element(css: '.gall_writer.ub-writer')
-                    reply_content = reply.find_element(css: '.usertxt.ub-word')
+          gif_images.each do |gif_image|
+            begin
+              download_image = URI.open(gif_image.css('source').attr('src'), 'User-Agent' => image_user_agent, 'Referrer' => post_url)
+              storage_board.images.attach(io: download_image, filename: SecureRandom.urlsafe_base64(20))
 
-                    StorageBoardCommentReply.create(
-                      storage_board_comment_id: storage_board_comment.id,
-                      nickname: reply_writer.attribute('data-nick'),
-                      content: reply_content.text,
-                      created_ip: reply_writer.attribute('data-ip'),
-                      is_member: 1
-                    )
+              gif_image.at('source')['src'] = storage_board.last_image_url
+              gif_image.at('source')['alt'] = 'Board Img'
+            rescue
+              next
+            end
+          end if gif_images.present?
+
+          storage_board.update(content: parse_storage_board_content, has_image: has_image)
+
+          storage_board_comment_url = "https://gall.dcinside.com/board/view/?id=#{storage.code}&no=#{scrap_code}&t=cv&exception_mode=recommend&page=1"
+
+          Selenium::WebDriver::Chrome::Service.driver_path = ENV['CHROME_DRIVER_PATH']
+
+          options = Selenium::WebDriver::Chrome::Options.new
+          options.add_argument("--user-agent=#{user_agent}")
+          options.headless!
+
+          browser = Selenium::WebDriver.for :chrome, options: options
+
+          begin
+            browser.get(storage_board_comment_url)
+
+            pages = browser.find_element(css: '.cmt_paging').find_elements(:xpath => "*")
+            page_nums = pages.map { |page| page.text.to_i }
+
+            page_nums.each do |page_num|
+              if page_num > 1
+                browser.execute_script("viewComments(#{page_num}, 'D')")
+                sleep 2
+              end
+
+              # Comments & Replies
+              browser.find_element(css: '.cmt_list').find_elements(:xpath => "*").each do |comment|
+                begin
+                  comment_data_no = comment.attribute('id').split('_').last
+                  comment_writer = comment.find_element(css: '.gall_writer.ub-writer')
+                  comment_content = comment.find_element(css: '.usertxt.ub-word')
+
+                  storage_board_comment = StorageBoardComment.create(
+                    storage_board_id: storage_board.id,
+                    nickname: comment_writer.attribute('data-nick'),
+                    content: comment_content.text,
+                    created_ip: comment_writer.attribute('data-ip'),
+                    is_member: 1
+                  )
+
+                  # Replies
+                  browser.find_elements(id: "reply_list_#{comment_data_no}").each do |reply_list|
+                    reply_list.find_elements(:xpath => "*").each do |reply|
+                      reply_writer = reply.find_element(css: '.gall_writer.ub-writer')
+                      reply_content = reply.find_element(css: '.usertxt.ub-word')
+
+                      StorageBoardCommentReply.create(
+                        storage_board_comment_id: storage_board_comment.id,
+                        nickname: reply_writer.attribute('data-nick'),
+                        content: reply_content.text,
+                        created_ip: reply_writer.attribute('data-ip'),
+                        is_member: 1
+                      )
+                    end
                   end
+                rescue
+                  next
                 end
-              rescue
-                next
               end
             end
+          ensure
+            browser.quit
           end
-        ensure
-          browser.quit
         end
-      end
 
-      if create_new_storage_board
-        namespace = "storage-#{storage.id}-boards"
-        Rails.cache.clear(namespace: namespace)
-      end
+        if create_new_storage_board
+          namespace = "storage-#{storage.id}-boards"
+          Rails.cache.clear(namespace: namespace)
+        end
+        end
     end
   end
 end
