80179a427d88ae51e9f4ae35652e1fea038acaca
kimjh96 <kimjhs@kakao.com>
2021-05-27 19:31:06 +0900

    fix: request host 에 따라 link 의 base_url 이 설정 되도록 RSS API 로직 수정

--- app/controllers/rss_controller.rb
+++ app/controllers/rss_controller.rb
@@ -1,22 +1,26 @@
 class RssController < ApplicationController
 
   def index
+    base_url = 'https://www.cocstorage.com'
+
+    base_url = 'https://cocstorage.com' if request.host.present? && request.referer.index('www.cocstorage.com').blank?
+
     storage_boards = StorageBoard.where(is_draft: false, is_active: true).order(id: :desc).limit(100)
 
     xml = "<?xml version='1.0' encoding='UTF-8'?>
     <rss version='2.0'>
       <channel>
         <title>개념글 저장소</title>
-        <link>https://www.cocstorage.com</link>
+        <link>#{base_url}</link>
         <description>내가 만들어 운영하는 커뮤니티 저장소</description>
         #{storage_boards.map do |storage_board|
            storage = Storage.find(storage_board.storage_id)
             "<item>
               <title>#{CGI.escapeHTML(storage_board.subject)}</title>
-              <link>https://www.cocstorage.com/storages/#{storage.path}/#{storage_board.id}</link>
+              <link>#{base_url}/storages/#{storage.path}/#{storage_board.id}</link>
               <description><![CDATA[#{CGI.escapeHTML(storage_board.description || '')[0..159]}]]></description>
               <pubDate>#{storage_board.created_at.rfc822}</pubDate>
-              <guid>https://www.cocstorage.com/storages/#{storage.path}/#{storage_board.id}</guid>
+              <guid>#{base_url}/storages/#{storage.path}/#{storage_board.id}</guid>
             </item>"
            end.join('')}
       </channel>
