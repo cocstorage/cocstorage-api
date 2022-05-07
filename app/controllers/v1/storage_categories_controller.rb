3412f8b77e710d8b9c8a7547232d176aed85bb6a
kimjh96 <kimjhs@kakao.com>
2022-05-07 17:52:58 +0900

    fix: 저장소 카테고리 목록 API 스펙 추가
    - 이슈 키워드 저장소 카테고리를 구분할 수 있도록 withIssueCategory 추가

--- app/controllers/v1/storage_categories_controller.rb
+++ app/controllers/v1/storage_categories_controller.rb
@@ -3,7 +3,23 @@ class V1::StorageCategoriesController < V1::BaseController
 
   def index
     render json: {
-      categories: StorageCategory.all
+      categories: StorageCategory.fetch_with_options(configure_index_params)
     }
+  end
+
+  private
+
+  def index_attributes
+    %w[withIssueCategory]
+  end
+
+  def configure_index_params
+    index_attributes.each do |key|
+      if params.key? key.to_sym
+        raise Errors::BadRequest.new(code: 'COC013', message: "#{key} is empty") if params[key.to_sym].blank?
+      end
+    end
+
+    params.permit(index_attributes)
   end
 end
--- app/models/storage_category.rb
+++ app/models/storage_category.rb
@@ -1,3 +1,11 @@
 class StorageCategory < ApplicationRecord
   has_many :storages
+
+  def self.fetch_with_options(options = {})
+    storageCategories = all
+
+    storageCategories = storageCategories.where.not(name: '이슈') unless options[:withIssueCategory].present?
+
+    storageCategories
+  end
 end
